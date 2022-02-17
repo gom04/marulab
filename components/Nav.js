157fa022744acec5a40f82420f9c524a914a5826
gom04 <jira.srt1986@gmail.com>
2022-02-17 17:50:30 +0700

    new

---
+++ components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react' 
+import Link from 'next/link'
+import Button from "react-bootstrap/Button";
+import { navLinks } from "../utils/request";
+
+function Nav() {
+  return (  
+<nav className='flex space-x-8   tracking-tight md:tracking-tighter leading-tight  mt-8'>
+  {navLinks.map((link, index) => {
+          return (
+            <ul>
+              <Link href={link.path}>
+                <li key={index}>{link.name}</li>
+              </Link>
+            </ul>
+          );
+        })}</nav>
+  )
+} 
+
+export default Nav 
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import { HomeIcon , LightningBoltIcon
+} from '@heroicons/react/outline'
+import HeaderItem from '../components/HeaderItem'
+import Nav from '../components/Nav'
+export default function Home() {
+  return ( 
+    <div className='flex flex-grow justify-evenly '> 
+    {/*
+  <HeaderItem title='HOME' Icon={HomeIcon} />
+  <HeaderItem title='PROJECT' Icon={LightningBoltIcon} />
+ header 
+<Header/>*/}
+<Header/>
+<Nav/>
+  {/*footer */}
