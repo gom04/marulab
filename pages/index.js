bc7a73d01a5364400e58e67f8e14095f952d1471
gom04 <jira.srt1986@gmail.com>
2022-02-17 18:38:38 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import { HomeIcon , LightningBoltIcon
-} from '@heroicons/react/outline'
+import { HomeIcon , LightningBoltIcon } from '@heroicons/react/outline'
 import HeaderItem from '../components/HeaderItem'
 import Nav from '../components/Nav'
 export default function Home() {
   return ( 
     <div className='flex flex-grow justify-evenly '> 
     {/*
-  <HeaderItem title='HOME' Icon={HomeIcon} />
-  <HeaderItem title='PROJECT' Icon={LightningBoltIcon} />
+
  header 
-<Header/>*/}
+<Header/>*/}  <HeaderItem title='HOME' Icon={HomeIcon} />
+  <HeaderItem title='PROJECT' Icon={LightningBoltIcon} />
 <Header/>
 <Nav/>
   {/*footer */}
